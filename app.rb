1295893d070a52c2c52b9b4c318a704970840705
Dmitriy <dimmak.260823@gmail.com>
2016-08-04 09:52:28 +0300

    Parse orders input, fix problem with cart on all pages

--- app.rb
+++ app.rb
@@ -19,5 +19,30 @@ get '/about' do
 end
 
 post '/cart' do
-  erb "Hello World"
+	orders_input = params[:orders]
+	erb "Hello World #{orders_input}"
+end
+
+def parse_orders_line orders_input
+
+	s1 = orders_input.split(',')
+
+	arr=[]
+
+	s1.each do |string|
+		arr2=[]
+		s2 = string.split('=')
+		s3 = s2[0].split('_')
+	
+		id = s3[1]
+		value = s2[1]
+		arr2.push(id)
+		arr2.push(value)
+		arr.push(arr2)
+	
+
+	end
+
+	return arr
+
 end
