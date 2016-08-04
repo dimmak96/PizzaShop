a1a0f5d3d8f96e919770ef0f624281261c362341
Dmitriy <dimmak.260823@gmail.com>
2016-08-05 01:55:32 +0300

    Add cancel button, add javascript handler stub

--- public/app.js
+++ public/app.js
@@ -69,3 +69,9 @@ function update_orders_button()
 	var text = 'Cart (' + cart_get_number_of_items() + ')';
 	$('#orders_button').val(text);
 }
+
+function cancel_order()
+{
+	alert('aaa');
+	return false;
+}
